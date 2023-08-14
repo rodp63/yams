944bab7379a2fbf0755e0405a56632cb39e30e58
Joaquin Palma Ugarte <joaquin.palma@ucsp.edu.pe>
2023-08-14 14:52:55 -0500

    Remove REDIS & add output option (#3)

--- yams/info.py
+++ yams/info.py
@@ -2,22 +2,11 @@
 news = {
     "name": "Newspapers",
     "env": {
-        "source": {
-            "value": "YAMS_NEWSPAPER",
-            "list": False,
-        },
-        "since": {
-            "value": "YAMS_NEWSPAPER_SINCE",
-            "list": False,
-        },
-        "to": {
-            "value": "YAMS_NEWSPAPER_TO",
-            "list": False,
-        },
-        "keywords": {
-            "value": "YAMS_NEWSPAPER_KEYWORDS",
-            "list": True,
-        },
+        "source": {"value": "YAMS_NEWSPAPER", "list": False},
+        "since": {"value": "YAMS_NEWSPAPER_SINCE", "list": False},
+        "to": {"value": "YAMS_NEWSPAPER_TO", "list": False},
+        "keywords": {"value": "YAMS_NEWSPAPER_KEYWORDS", "list": True},
+        "output": {"value": "YAMS_NEWSPAPER_OUTPUT", "list": False},
     },
     "spiders": ["diariocorreo", "elcomercio", "peru21"],
 }
